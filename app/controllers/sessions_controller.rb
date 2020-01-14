211eb9c904ac24a2a9bacb1c5274526b8e99175e
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-14 17:07:48 +0900

    8.1 セッション

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
+  end
+
+  def destroy
+    
+  end
+end
