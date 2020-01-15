f831093f8a5f0ec498d03d4e236318f0d0e60293
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-15 10:48:20 +0900

    8.2.5 ユーザー登録時にログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       #       redirect_to  “/users/#{@user.id}”
       # ↓users _path  POST   /users(.:format)          users#create
@@ -27,6 +28,8 @@ class UsersController < ApplicationController
     end
     
   end
+
+
 
   private
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,12 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:michael)
+  end
+  # 上のusersはfixtureのファイル名users.ymlを表し、
+  # :michaelというシンボルはリスト 8.22のユーザーを参照するためのキーを表します。
+
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
@@ -11,4 +17,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, 
+                                          password: 'password' } } 
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
