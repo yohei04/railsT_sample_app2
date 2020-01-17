1edb1a26582888f97373b5a0f1e1d0741fea78b3
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-17 15:30:49 +0900

    10.1.4 TDDで編集を成功させる

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,15 +7,37 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     
   end
 
-
   test "unsuccessful edit" do
-    get edit_user_path @user
+    get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path, params: { user: { name: "",
+    patch user_path(@user), params: { user: { name: "",
                                             email: "user@invalid",
                                             password: "foo",
                                             password_confirmation: "bar" } }
     assert_template 'users/edit'
     assert_select "div.alert", "The form contains 4 errors."
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                       email: email,
+                                       password: "",
+                                       password_confirmation: "" } }
+    # ↓自分で書いた方
+    # follow_redirect! 
+    # assert_template 'users/show'
+    # assert_not flash.empty?
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+
+  end
+
+
 end
