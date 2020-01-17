0b6a6f8f4c321a3f91b0520777c5d0a392b24849
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-17 18:47:37 +0900

    10.2.1 ユーザーにログインを要求する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+
 
   def show
     @user = User.find(params[:id])
@@ -48,6 +49,14 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # before action
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
 
