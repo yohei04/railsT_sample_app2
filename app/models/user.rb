e56a73a2372684982ba3a2cf4105c9195d08ae73
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-21 15:50:58 +0900

    13.1.3 User/Micropostの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts
+
   attr_accessor :remember_token, :activation_token, :reset_token
   # before_save { self.email = self.email.downcase }
   before_save :downcase_email
