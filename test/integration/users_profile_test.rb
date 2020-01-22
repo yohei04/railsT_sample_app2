ab48a7ce1f20918f99e61869554b110f76335b0d
yoheihigashi04 <shacy.left04@gmail.com>
2020-01-22 22:45:09 +0900

    14.2.3 [Following] と [Followers] ページ

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,4 +19,11 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
   end
+
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
 end
